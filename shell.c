46d0d6614ba3b311804a3f80524a427553a5bbe4
wxr <xiruo.wang@sjtu.edu.cn>
2023-10-04 00:15:03 +0800

    joj. p1m3-0

--- shell.c
+++ shell.c
@@ -56,14 +56,45 @@ void command_echo(void){
     }
 }
 
-void command_cat(char file[]){
-    //printf("test:%s",file);
+//void command_cat(char file[]){
+//    //printf("test:%s",file);
+//    int find = 0;
+//    for(int i = 0; i < tot_file; i++){
+//
+//        //printf("file is %s %s",files[i].name,files[i].content);
+//        if(strcmp(file, files[i].name) == 0){
+//            printf("%s ",files[i].content);
+//            find = 1;
+//            break;
+//        }
+//    }
+//    if(find == 0){
+//        printf("error: no this file");
+//    }
+//}
+char* command_cat(char file[]){
+    char* lines = (char*)malloc(30 * sizeof(char));
     int find = 0;
+    int length = 0;
     for(int i = 0; i < tot_file; i++){
-
-        //printf("file is %s %s",files[i].name,files[i].content);
         if(strcmp(file, files[i].name) == 0){
-            printf("%s ",files[i].content);
+            char *sect;
+            sect = NULL;
+            sect = strtok(files[i].content, "\n");
+
+            while(sect){
+                if(length == 0){
+                    strcpy(lines,sect);
+                    strcat(lines,"\n");
+                    length++;
+                }
+                else if(length < 30){
+                    strcat(lines,sect);
+                    strcat(lines,"\n");
+                    length++;
+                }
+                sect = strtok(NULL,"\n");
+            }
             find = 1;
             break;
         }
@@ -71,35 +102,14 @@ void command_cat(char file[]){
     if(find == 0){
         printf("error: no this file");
     }
+    return lines;
 }
 
 char* command_head(char file[]){
     char* lines = (char*)malloc(10 * sizeof(char));
     int find = 0;
     int length = 0;
-
-//    for(int i = 0; i < tot_file; i++){
-//        //printf("file is %s %s",files[i].name,files[i].content);
-//        if(strcmp(file, files[i].name) == 0){
-//            char *sect;
-//            sect = NULL;
-//            sect = strtok(files[i].content, "\n");
-//            while(sect){
-//                strcpy(ten_line[line],sect);
-//                line++;
-//                sect = strtok(NULL,"\n");
-//            }
-//            for(int k = 0; k < line && k < 10; k++){
-//                //printf("%d:",line);
-//                printf(ten_line[k]);
-//                printf("\n");
-//            }
-//            find = 1;
-//            break;
-//        }
-//    }
     for(int i = 0; i < tot_file; i++){
-        //printf("file is %s %s",files[i].name,files[i].content);
         if(strcmp(file, files[i].name) == 0){
             char *sect;
             sect = NULL;
@@ -128,7 +138,75 @@ char* command_head(char file[]){
     return lines;
 }
 
+char*  command_diff(char file_a[],char file_b[]){
+    char* lines = (char*)malloc(10 * sizeof(char));
+    int find = 0;
+    int length = 0;
+    int a_index = 0;
+    int b_index = 0;
+    for(int i = 0; i < tot_file && find != 2; i++){
+        if(strcmp(file_a, files[i].name) == 0){
+            a_index = i;
+            find++;
+        }
+        if(strcmp(file_b, files[i].name) == 0){
+            b_index = i;
+            find++;
+        }
+    }
+    if(find == 0){
+        printf("error: no this file");
+    }
+    char *sect_a;
+    sect_a = NULL;
+    sect_a = strtok(files[a_index].content, "\n");
+
+    char *sect_b;
+    sect_b = NULL;
+    sect_b = strtok(files[b_index].content, "\n");
+
+    while(sect_a && sect_b){
+        if(length == 0){
+            strcpy(lines,sect_a);
+            strcat(lines,"\n");
+            if(strcmp(sect_a,sect_b) != 0){
+                strcat(lines,"| ");
+            }
+            strcat(lines,sect_b);
+            strcat(lines,"\n");
+            length++;
+        }
+        else if(length < 10){
+            strcat(lines,sect_a);
+            strcat(lines,"\n");
+            if(strcmp(sect_a,sect_b) != 0){
+                strcat(lines,"| ");
+            }
+            strcat(lines,sect_b);
+            strcat(lines,"\n");
+            length++;
+        }
+        sect_a = strtok(NULL,"\n");
+        sect_b = strtok(NULL,"\n");
+    }
+    while(sect_a){
+        strcat(lines,sect_a);
+        strcat(lines,"\n");
+        strcat(lines,"<\n");
+        sect_a = strtok(NULL,"\n");
+    }
+    while(sect_b){
+        strcat(lines,"> ");
+        strcat(lines,sect_b);
+        strcat(lines,"\n");
+        sect_b = strtok(NULL,"\n");
+    }
+    return lines;
+
+}
+
 void command_output_redirect(char file_name[], char content[],int cases){
+    //Write content to a file, attach or overwrite
     int find = 0;
     for(int i = 0; i < tot_file; i++){
         if(strcmp(file_name, files[i].name)==0){
@@ -157,30 +235,6 @@ void command_output_redirect(char file_name[], char content[],int cases){
     }
 }
 
-void command_input_redirect(char in_file_name[],char out_file_name[],int cases, int length){
-    int find = 0;
-    char  content[1024];
-
-    for(int i = 0; i < tot_file; i++){
-        if(strcmp(in_file_name, files[i].name) == 0){
-            if(length == 100){
-                strcpy(content,files[i].content);
-            }
-            else if(length == 10){
-
-            }
-
-            find = 1;
-        }
-    }
-    if(find == 0){
-        printf("error: input file not found");
-    }
-    else{
-        command_output_redirect(out_file_name,content,cases);
-    }
-
-}
 
 int execute(void){
     char* output = NULL;
@@ -217,7 +271,9 @@ int execute(void){
             return 1;
         }
         else if(strcmp(argv[0], "cat") == 0){
-            command_cat(argv[1]);
+            char* outs = command_cat(argv[1]);
+            printf("%s", outs);
+            free(outs);
             return 1;
         }
         else if(strcmp(argv[0], "head") == 0){
@@ -229,13 +285,19 @@ int execute(void){
     }
     else if(tot_argv == 3){
         if(strcmp(argv[0], "cat") == 0 && strcmp(argv[1], "<") == 0) {
-            command_cat(argv[2]);
+            char* outs = command_cat(argv[2]);
+            printf("%s", outs);
+            free(outs);
             return 1;
         }
         else if(strcmp(argv[0], "head") == 0 && strcmp(argv[1], "<") == 0){
             char* outs = command_head(argv[2]);
             printf("%s", outs);
             free(outs);
+            return 1;
+        }
+        else if(strcmp(argv[0], "echo") == 0){
+            command_echo();
             return 1;
         }
     }
@@ -249,8 +311,8 @@ int execute(void){
             return 1;
         }
         else if(strcmp(argv[0], "cat") == 0){
-            char in_file_name[50];
             char out_file_name[50];
+            char* outs;
             int cases = 0;
             for(int i = 1; i < tot_argv; i++){
                 if(strcmp(argv[i],">") == 0){
@@ -262,10 +324,11 @@ int execute(void){
                     cases = 1;
                 }
                 else if(strcmp(argv[i],"<") == 0){
-                    strcpy(in_file_name , argv[i+1]);
+                    outs = command_head(argv[2]);
                 }
             }
-            command_input_redirect(in_file_name,out_file_name,cases,50);
+            command_output_redirect(out_file_name,outs,cases);
+            free(outs);
             return 1;
         }
         else if(strcmp(argv[0], "head") == 0){
@@ -284,11 +347,21 @@ int execute(void){
                 else if(strcmp(argv[i],"<") == 0){
                      outs = command_head(argv[i+1]);
                     //printf("%s", outs);
-
                 }
             }
             command_output_redirect(out_file_name,outs,cases);
             free(outs);
+            return 1;
+        }
+        else if(strcmp(argv[0], "diff") == 0){
+            for(int i = 1; i < tot_argv; i++){
+                if(strcmp(argv[i],"<") == 0){
+                    char* outs = command_diff(argv[i-1],argv[i+1]);
+                    printf("%s",outs);
+                    free(outs);
+                    break;
+                }
+            }
             return 1;
         }
         else if(strcmp(argv[0], "echo") == 0){
